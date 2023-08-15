82b237dc8faa4594e234bf801329f2e6b5e6b56b
SamiraMiss <samirarostami71@gmail.com>
2023-08-15 21:03:28 +0330

    Edited slider, banner and projects section.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const menuBtn = document.querySelector(".nav__toggler");
 const navCollaps = document.querySelector(".collapsible");
+const searchBtn = document.querySelector(".btn--search");
+const searchBox = document.querySelector(".search-box");
 
 menuBtn.addEventListener("click", function () {
   navCollaps.classList.toggle("collapsible--expanded");
+});
+
+searchBtn.addEventListener("click", function () {
+  document.body.classList.add("opened");
+  searchBox.style.display = "block";
+});
+
+document.querySelector(".btnClose").addEventListener("click", function (e) {
+  searchBox.style.display = "none";
 });
